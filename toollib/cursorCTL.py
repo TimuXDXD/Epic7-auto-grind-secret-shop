0a91b325b4ca2b40b72b88a0f56ac333ec7278b0
Timu <timu890518@gmail.com>
2023-06-19 01:59:54 +0800

    Update tool

--- toollib/cursorCTL.py
+++ toollib/cursorCTL.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import win32gui
-from grabScreen import getScreenSize
+from frame_op import getScreenSize
 
 def calCursorPos(hwnd):
     left, top, right, bottom = win32gui.GetWindowRect(hwnd)
